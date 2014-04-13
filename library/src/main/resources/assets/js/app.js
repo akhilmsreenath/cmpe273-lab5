4e57b62c14f3a2480f7751604b4eff6787ab6b06
akhilmsreenath <akhilmsreenath@gmail.com>
2014-04-12 21:11:43 -0700

    lab 5

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,24 @@
 $(":button").click(function() {
-	var isbn = this.id;
-	alert('About to report lost on ISBN ' + isbn);
+
+    var isbn = this.id;
+
+    alert('About to report lost on ISBN ' + isbn);
+    $.ajax({
+    	url: 'http://localhost:8001/library/v1/books/'+isbn+'?status=lost',
+    	type: 'PUT',
+    		
+    		//data :"status:lost",
+    			success: fuction(data){
+    	$("#status_"+isbn).html("lost");
+    	$("#"+isbn).attr("disabled","disabled");
+    	
+    				
+    				//alert('lost.');
+    			}
+    	
+    }
+    	
+    );
+
 });
+
